c9887d8740c2739260c6503839cffb8a62eb904a
Sean Harder <>
2026-01-23 15:11:19 -0500

    [01.23.26][SH] Update Files

--- project_files/src/Frontend/Pages/AllFlightsPage.tsx
+++ project_files/src/Frontend/Pages/AllFlightsPage.tsx
@@ -3,6 +3,11 @@ import react from 'react';
 
 const AllFlightsPage = (() => {
 
+     return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    );
 })
 
 export default AllFlightsPage;
