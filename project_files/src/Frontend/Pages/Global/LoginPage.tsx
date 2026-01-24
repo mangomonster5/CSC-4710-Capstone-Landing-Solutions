b9be5579f59c3a65b754acd0df2bfd6689947c52
Sean Harder <seanharder22@gmail.com>
2026-01-23 21:53:40 -0500

    [01.23.26][SH] Update - Read More
    1. Set up the frontend routing
    2. Set up frontend folder structure
    3. Implemented navbar component
    4. Implemented AllFlightsPage.tsx

--- project_files/src/Frontend/Pages/Global/LoginPage.tsx
+++ project_files/src/Frontend/Pages/Global/LoginPage.tsx
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router-dom";
 
 
-const LoginPage = (() => {
+
+
+const LoginPage: React.FC = () => {
+    const navigate = useNavigate();
+
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="w-100 mt-4 d-flex flex-column align-items-center gap-4">
+            <div>
+                <h1 className="text-center">Login Page</h1>
+                <p className="text-center mb-0">This page has not been implemented yet</p>
+            </div>
+            <button className="rounded border py-1 text-white" style={{ width: '150px', background: '#8C52FF' }} onClick={() => navigate('/all-flights', { replace: true })}>Help</button>
         </div>
     );
-
-
-})
+}
 
 export default LoginPage;
