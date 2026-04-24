e8cd5328dba046c476bd93aebb2ba384c6cabf03
Kamryn Wagner <wkamryn26@gmail.com>
2026-04-24 01:40:55 -0400

    the mf flight selection

--- project_files/Frontend/src/GlobalComponents/FlightSelectionDropdown.tsx
+++ project_files/Frontend/src/GlobalComponents/FlightSelectionDropdown.tsx
@@ -1,15 +1,17 @@
 import React, { ReactNode } from 'react';
 import useAllStateContext from '../context/useAllStateContext';
+
 interface FlightSelectionDropdownProps {
-    handleSelection: (selection: any) => void
+    handleSelection: (selection: any) => void;
     body: ReactNode;
+    excludeAirport?: Airport | null;
 }
 
-const FlightSelectionDropdown: React.FC<FlightSelectionDropdownProps> = ({ handleSelection, body }) => {
-    const { allAirports } = useAllStateContext()
+const FlightSelectionDropdown: React.FC<FlightSelectionDropdownProps> = ({ handleSelection, body, excludeAirport }) => {
+    const { allAirports } = useAllStateContext();
 
     return (
-        <div className="dropdown ">
+        <div className="dropdown">
             <div
                 className="card d-flex flex-column no-i-cursor gap-1 dropdown-toggle text-wrap"
                 data-bs-toggle="dropdown"
@@ -23,20 +25,21 @@ const FlightSelectionDropdown: React.FC<FlightSelectionDropdownProps> = ({ handl
                 style={{ maxHeight: '200px', overflowY: 'auto' }}
             >
                 {allAirports == null ? (
-                    <>No Airports</>
+                    <li className="dropdown-item text-muted">No Airports</li>
                 ) : (
-                    <>
-                        {allAirports.map((airport: Airport) => (
-                            <li key={airport.iata_code} onClick={() => handleSelection(airport)} style={{ cursor: "pointer" }}>
+                    allAirports
+                        .filter((airport: Airport) => airport.airport_id !== excludeAirport?.airport_id)
+                        .map((airport: Airport) => (
+                            <li key={airport.iata_code} onClick={() => handleSelection(airport)} style={{ cursor: 'pointer' }}>
                                 <div className="dropdown-item">
                                     <span className="fw-bold">[{airport.iata_code}]</span> {airport.name}
                                 </div>
                             </li>
-                        ))}
-                    </>
+                        ))
                 )}
             </ul>
         </div>
-    )
-}
+    );
+};
+
 export default FlightSelectionDropdown;
